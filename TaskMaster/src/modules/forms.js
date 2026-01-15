266b00573e7cfb9681a422a790f885b2cbce6f62
Tomas Bohumil Sobotka <sobotkatomas@sssvt.cz>
2026-01-15 09:38:36 +0100

    update: TaskMaster JavaScript, new delivery_form_project

--- TaskMaster/src/modules/forms.js
+++ TaskMaster/src/modules/forms.js
@@ -29,7 +29,50 @@ export function InitializeFormData() {
         });*/
 
         taskNameEl.addEventListener("submit", (e) => {
-
+            console.log(taskNameEl.value);
         })
     });
+
+
+    form.addEventListener("input", () => {
+        const errors = validateForm();
+
+        if (errors && errors.length > 0) {
+            console.log(errors.join(","));
+            buttonEl.disabled = true;
+        } else {
+            buttonEl.disabled = false;
+        }
+    })
+}
+
+
+function validateForm() {
+    let errors = [];
+    const data = new FormData(form);
+
+    if (data.get("task") && data.get("task").length <= 3) {
+        errors.push("Task name must be at least 3 characters.");
+    }
+
+    //zamezit datum v minulosti
+    const today = Date.now();
+    console.log(today);
+    if (data.get("datetime-local") && data.get("datetime-local") < today || data.get("datetime-local") == null) {
+        errors.push("Date is in the past.");
+    }
+    return errors;
+}
+
+
+
+
+function getFormDataAsObject() {
+    const data = new FormData(form);
+
+    const res = {};
+    for (let pair of data.entries()){
+        res[pair[0]] = pair[1];
+    }
+    return res;
 }
